c5ca3d932f1fdb821001cca36d1911456eb2997b
DESKTOP-VPA0G71\ksh95 <ksh950609@gmail.com>
2024-12-24 18:05:47 +0900

    [2024-12-24 김상훈] 프로젝트 확장을 위한 postgreSql 연결과 메인 페이지 생성
    페이지 기획하기
    (대략)
    게시판 만들기
    -> 공지, 문의하기 만들기
    
    통계 페이지 만들기
    
    아무 데이터 들고 와서 만들자 (그래프) -> 뭐로 할건지 그리고 주제는 뭐로 할 것인지
    
    지도 페이지 만들기 - 지역별 데이터를 받아서 표시하기 ? 아니면 길 안내 ? 맛집 찾기?
    
    - 뭐 깔쌈한 아이디어 없을까...
    WEB API 사용
    (재미있는거 하나 만들자잉) -> 이거는 재미있어 보이는거 넣을 예정
    
    금융 정보 확인하기
    페이지 두개 만들어서 (주식) , (가상자산)
    
    -> 금융정보 있는데 투자관련으로 만들어볼까?

--- sbb/src/main/java/com/example/demo/MainController.java
+++ sbb/src/main/java/com/example/demo/MainController.java
@@ -1,19 +1,34 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
-    @GetMapping("/sbb")
-    @ResponseBody 
-    public String index() {
-        return "question";
-    }
+//    @GetMapping("/sbb")
+//    @ResponseBody 
+//    public String index() {
+//        return "question";
+//    }
+	@RequestMapping("/main")
+	public ModelAndView viewMain(@RequestParam Map<String, Object> paramsMap, ModelAndView mav,
+			HttpServletRequest request, HttpServletResponse response) throws DataNotFoundException {
+		mav.setViewName("/main");
+		mav.addObject("error", request.getAttribute("error"));
+		return mav;
+	}
     
-    @GetMapping("/")
-    public String root() {
-        return "redirect:/question/list";
-    }
+//    @GetMapping("/")
+//    public String root() {
+//        return "redirect:/question/list";
+//    }
 }
